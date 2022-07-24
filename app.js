39b1868ae3b13c918be275d1a02c60b493cc9594
Vit Mur <myp1blka@gmail.com>
2022-07-25 02:11:43 +0300

    start

---
+++ app.js
@@ -0,0 +1,21 @@
+
+
+const fs = require('fs/promises');
+const os = require('os');
+
+const init = async () => {
+    //await fs.mkdir(`${__dirname}/abc2/`)
+    //console.log(__dirname);
+
+    console.log(os.cpus());
+
+    console.log('it work');
+}
+'є'
+
+init();
+
+
+
+
+
---
+++ test.js
@@ -0,0 +1,19 @@
+
+console.log('test' + ' + test + test');
+
+let bb = 20;
+// bb = 20; не юзать бел let или const
+
+const dd = 30;
+module.exports = {
+    dd,
+    ff: 40
+}
+
+
+
+
+
+
+
+
