91689664878072e974efe494b16cd13311864f95
Vit Mur <myp1blka@gmail.com>
2022-07-25 03:38:03 +0300

    weather not work 1

--- app.js
+++ app.js
@@ -1,21 +1,35 @@
-
-
 const fs = require('fs');
 const moment = require('moment'); 
-
+const weather = require('weather-js');
 const init = async () => {
 
-    await fs.writeFile(`${moment().format('X')}.log`, `Погода в Києві`, function (err)    {
+    let myWeather = weather.find({search: 'Kyev, UA', degreeType: 'C'}, function(err, result) {
+        if(err) console.log(err);
+        console.log(result);
+        console.log(' === ');
+        console.log(result[0].location.name);
+      });
+
+      console.log("1111111111111" + myWeather[0].data);
+       let addInfoToFile = `у`;
+    //    ${myWeather[0].location.name} , сьогодні 
+    //    ${myWeather[0].current.temperature} градусів
+    //    `;
+
+
+    await fs.writeFile(`${moment().format('X')}.log`, `${addInfoToFile}`, function (err){
         if (err) throw err;
         console.log('File is created successfully.');
       });
-
-
     console.log('it work');
 }
-
-
 init();
+
+
+
+
+// Когда использовать node start а когда npm start
+// как работать с бибдиотекой moment
 
 
 
